c41e808b087ab6f13fc68c190acda6c52f7f81ca
David Johansson <119085651+djoh826@users.noreply.github.com>
2024-01-07 18:00:03 -0600

    Home update
    Updated hero with links and captions. Added slider for sale items as well

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,40 +1,65 @@
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 // import Navbar from './Navbar.js';
 import './Home.css';
 import hero1 from '../Resources/t1.jpg';
 import hero2 from '../Resources/t3.jpg';
 import hero3 from '../Resources/t4.jpg';
+import ScrollingRender from './ScrollingRender.js';
 
 function Home() {
   return (
-    //Alert Banner
-    //Navbar 1 (logo,,acct,cart)
-    //Navbar 2 (sale - apparel - accessories - furniture)
-
-    //Hero - row of 3 things, (the 3 categories, but with captions)
-    //flexbox/grid?
+    //Alert Banner?
 
     //Feature 1 (FeatureCard.js)
-    //Sale stuff (Scrolling bar)
     //Feature 2
 
     //Footer
     <body style={{ padding: '2% 5% 5% 5%' }}>
       <div className="hero">
         <div className="left-half">
-          <img src={hero1} alt="Some pillows on a bedside" />
+          <Link to="/furniture">
+            <img
+              src={hero1}
+              alt="Some pillows on a bedside"
+              className="hover-anim"
+            />
+          </Link>
+          <div className="caption">Modern Furnishing</div>
         </div>
         <div className="right-half">
           <div className="right-top-right">
-            <img src={hero2} alt="Two smiling women wearing sweaters" />
+            <Link to="/apparel">
+              <img
+                src={hero2}
+                alt="Two smiling women wearing sweaters"
+                className="hover-anim"
+              />
+            </Link>
+            <div className="caption">Fashionable Clothing</div>
           </div>
           <div className="right-bottom-right">
-            <img
-              src={hero3}
-              alt="A stool surrounded by yarn-knit accessories"
-            />
+            <Link to="/accessories">
+              <img
+                src={hero3}
+                alt="A stool surrounded by yarn-knit accessories"
+                className="hover-anim"
+              />
+            </Link>
+            <div className="caption">Stylish Accessories</div>
           </div>
         </div>
+      </div>
+
+      <h1 style={{ margin: '2.5% 0 1% 0' }}>Check out our sale!</h1>
+      <div
+        className="sale-section"
+        style={{
+          overflowX: 'scroll',
+          overflowY: 'hidden',
+          display: 'flex',
+          padding: '0 0 0 25%', //"Magic number" fix
+        }}>
+        <ScrollingRender />
       </div>
     </body>
   );
---
+++ src/Components/ScrollingRender.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import productsData from './items.json';
+
+const ProductRender = () => {
+  // Filter products based on tag
+
+  const tag = 'sale';
+  var filteredProducts;
+
+  if (tag === '') {
+    filteredProducts = productsData;
+  } else {
+    filteredProducts = productsData.filter(
+      (product) => product.tags && product.tags.includes(tag)
+    );
+  }
+
+  const productCards = filteredProducts.map((product) => (
+    <div
+      key={product.name}
+      className="product-card"
+      style={{ border: '1px solid rgb(124, 191, 236)', margin: '5px' }}>
+      <img
+        src={process.env.PUBLIC_URL + `/${product.image}`}
+        alt={product.name}
+        className="product-image"
+        style={{
+          maxWidth: '250px',
+          height: 'auto',
+          margin: '2px',
+        }}
+      />
+      <div
+        className="product-details"
+        style={{
+          display: 'block',
+          textAlign: 'center',
+          alignItems: 'center',
+        }}>
+        <h3 style={{ fontSize: '1.4rem' }}>{product.name}</h3>
+        <p className="product-price" style={{}}>
+          {product.tags.includes('sale') ? (
+            <>
+              <span style={{ textDecoration: 'line-through' }}>
+                ${product.price}
+              </span>
+              <span style={{ fontWeight: 'bold' }}> ${product.salePrice}</span>
+            </>
+          ) : (
+            `$${product.price}`
+          )}
+        </p>
+      </div>
+    </div>
+  ));
+
+  const itemsPerRow = 100;
+
+  const rows = [];
+  for (let i = 0; i < productCards.length; i += itemsPerRow) {
+    rows.push(
+      <div
+        key={`row-${i / itemsPerRow}`}
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          maxWidth: '100%',
+          maxHeight: '100%',
+        }}>
+        {productCards.slice(i, i + itemsPerRow)}
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className="product-container"
+      style={{ maxWidth: '100%', margin: '1% 10%' }}>
+      {rows}
+    </div>
+  );
+};
+
+export default ProductRender;
