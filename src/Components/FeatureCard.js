fbbac3624278204c785c50965a4038851c64d11d
David Johansson <119085651+djoh826@users.noreply.github.com>
2024-01-19 00:37:24 -0600

    Update FeatureCard

--- src/Components/FeatureCard.js
+++ src/Components/FeatureCard.js
@@ -1,4 +1,5 @@
 import './FeatureCard.css';
+import { Link } from 'react-router-dom';
 
 function FeatureCard() {
   return (
@@ -9,11 +10,16 @@ function FeatureCard() {
           alt="A stylish woman sitting comfortably looking across."></img>
       </div>
       <div className="feature-right-half">
-        <h2>Style meets comfort</h2>
-        <p>
-          Comfy Market's collection of apparel is hand-made for purpose of
-          combining the best of both worlds: comfort and style.
-        </p>
+        <div className="right-container">
+          <h2>Style meets comfort</h2>
+          <p>
+            Comfy Market's collection of apparel is hand-made for purpose of
+            combining the best of both worlds: comfort and style.
+          </p>
+          <Link to="/shop/apparel" className="center-container">
+            <button>Shop Apparel</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
